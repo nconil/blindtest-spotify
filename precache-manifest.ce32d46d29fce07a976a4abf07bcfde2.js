881a463bccef6e0cbd34ad56abb36fbfaea544f5
nconil <nicolas.conil@free.fr>
2019-03-05 20:11:10 +0100

    Updates

---
+++ precache-manifest.ce32d46d29fce07a976a4abf07bcfde2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "2bed69904f22342c89d6",
+    "url": "/blindtest-spotify/static/js/runtime~main.2bed6990.js"
+  },
+  {
+    "revision": "21498f884791afad2367",
+    "url": "/blindtest-spotify/static/js/main.21498f88.chunk.js"
+  },
+  {
+    "revision": "e588a188afcb1da3b07a",
+    "url": "/blindtest-spotify/static/js/2.e588a188.chunk.js"
+  },
+  {
+    "revision": "21498f884791afad2367",
+    "url": "/blindtest-spotify/static/css/main.6ec6af19.chunk.css"
+  },
+  {
+    "revision": "000aa15eb8a2de8fa31a6d3ff73f47c4",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
